1eda10381844551de07d665e967c501d6dc42db4
FaustVX <jodu035@gmail.com>
2025-01-25 17:30:04 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Spectre.Console;
+
+AnsiConsole.MarkupLine("[underline red]Hello[/] World!");
+// Ask for the user's favorite fruit
+var fruit = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .Title("What's your [green]favorite fruit[/]?")
+        .PageSize(5)
+        .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+        .AddChoices([
+            "Apple", "Apricot", "Avocado", 
+            "Banana", "Blackcurrant", "Blueberry",
+            "Cherry", "Cloudberry", "Cocunut",
+        ]));
+
+// Echo the fruit back to the terminal
+AnsiConsole.WriteLine($"I agree. {fruit} is tasty!");
